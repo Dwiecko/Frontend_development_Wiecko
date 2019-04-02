7cf49173ac2281b1a85d2fd5918babf79952006e
dwiecko <dwiecko@sigma.ug.edu.pl>
2019-04-02 21:35:22 +0200

    Public print camera production year function

--- Lab3/main.js
+++ Lab3/main.js
@@ -33,30 +33,30 @@ Camera.prototype = {
 // Public methods
 Camera.prototype.getProducer = function()
 {
-    return this.Producer;
+    return producer;
 }
 Camera.prototype.getModel = function()
 {
-    return this.model;
+    return model;
 }
 Camera.prototype.getProductionYear = function()
 {
-    return this.productionYear;
+    return productionYear;
 }
 Camera.prototype.getUsb20 = function()
 {
-    return this.usb20;
+    return usb20;
 }
 
 //private
-function addSingleCamera(producer, model, productionYear, usb20, price)
+function addSingleCamera(_producer, _model, _productionYear, _usb20, _price)
 {
     return {
-        Producer: producer,
-        Model: model,
-        ProductionYear: productionYear,
-        USB20: usb20,
-        Price: price
+        producer: _producer,
+        model: _model,
+        productionYear: _productionYear,
+        usb20: _usb20,
+        price: _price
     };
 }
 
@@ -82,12 +82,11 @@ function Camera(_producer, _model, _productionYear, _usb20, _price)
     printCamera();
 }
 
-// Object invoke
 Camera.prototype = Object.create(Camera.prototype);
 Camera.prototype.constructor = Camera;
 
 var camerasClass = new Camera("Panasonic", "Lumix AB-ABCD2", 2017, true, 20000.00);
-console.log(camerasClass.getProductionYear());
+console.log("Public method, get production year: " + camerasClass.getProductionYear());
 
 var secondCamera = new Camera("Lumix", "Lumix AB-ABCD2", 2017, true, 20000.00)
 camerasArray.push(secondCamera);
