e1f6399a445661022c995cb30f75f4e28fb24b24
dwiecko <dwiecko@sigma.ug.edu.pl>
2019-05-12 09:42:26 +0200

    Add files from lab

--- lab7/Server/index.js
+++ lab7/Server/index.js
@@ -3,17 +3,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const port = 4000;
+const camerasCatalogue = new CamerasCatalogue();
 
 app.use(bodyParser.json());
-const cameras = [];
 
 app.get("/api/", (req, res) => res.send("Hi there"));
 
-app.get("/api/cameras/all", (req, res) => res.send(cameras));
+app.get("/api/cameras/all", (req, res) =>
+  res.send(camerasCatalogue.getAllCameras())
+);
+
+app.get("/api/cameras/get", (req, res) => {
+  const cameraModel = req.body.cameraModel;
+
+  res.send(camerasCatalogue.getAllCameras());
+});
 
 app.post("/api/cameras", (req, res) => {
-  const camera = req.body.camera;
-  cameras.push(camera);
+  camerasCatalogue.addCamera(req.camera);
   res.send({ message: "camera was added" });
 });
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
