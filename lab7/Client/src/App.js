f3fcc2fe09d49a63d2422b19238ef3ae94b66a93
dwiecko <dwiecko@sigma.ug.edu.pl>
2019-05-13 22:28:04 +0200

    Seed data

--- lab7/Client/src/App.js
+++ lab7/Client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Camera from "./Components/Camera";
-import CamerasCatalogue from "./Service/CamerasCatalogue";
+import CamerasCatalogue from "./Containers/CamerasCatalogue";
 
 class App extends Component {
   constructor(props) {
@@ -9,6 +9,15 @@ class App extends Component {
 
     this.camerasDb = new CamerasCatalogue();
 
+    this.seedData();
+
+    this.state = {
+      allCameras: this.camerasDb.getAllCameras(),
+      randomCameraIndex: 0
+    };
+  }
+
+  seedData() {
     this.camerasDb.addCamera(
       new Camera("Nikon", "RX 3000", 2000, true, 2000.99)
     );
@@ -24,10 +33,6 @@ class App extends Component {
     this.camerasDb.addCamera(
       new Camera("Sony", "Alfa 180", 2000, false, 7000.99)
     );
-    this.state = {
-      allCameras: this.camerasDb.getAllCameras(),
-      randomCameraIndex: 0
-    };
   }
 
   getRandomCameraIndex() {
@@ -54,43 +59,51 @@ class App extends Component {
         <div className="allCameras">
           <h2>All cameras:</h2>
           <table>
-            <tr>
-              <th>Model</th>
-              <th>Producer</th>
-              <th>ProductionYear</th>
-              <th>USB20</th>
-              <th>Price</th>
-            </tr>
-
-            {this.state.allCameras.map(camera => (
-              <tr key={camera.toString()}>
-                <td>{camera.model}</td>
-                <td>{camera.producer}</td>
-                <td>{camera.productionYear}</td>
-                <td>{camera.USB20}</td>
-                <td>{camera.price}</td>
+            <thead>
+              <tr>
+                <th>Model</th>
+                <th>Producer</th>
+                <th>Production Year</th>
+                <th>USB20</th>
+                <th>Price</th>
               </tr>
-            ))}
+            </thead>
+            <tbody>
+              {this.state.allCameras.map(camera => (
+                <tr key={camera.toString()}>
+                  <td>{camera.model}</td>
+                  <td>{camera.producer}</td>
+                  <td>{camera.productionYear}</td>
+                  <td>{camera.USB20}</td>
+                  <td>{camera.price}</td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
+
         <div className="randomCamera">
           <h2>
             Random camera array index from 0 to{" "}
             {this.state.allCameras.length - 1}: {this.state.randomCameraIndex}
           </h2>
           <table key={this.state.randomCameraIndex}>
-            <tr>
-              <th>Model</th>
-              <th>Producer</th>
-            </tr>
-            <tr>
-              <td>
-                {this.state.allCameras[this.state.randomCameraIndex].model}
-              </td>
-              <td>
-                {this.state.allCameras[this.state.randomCameraIndex].producer}
-              </td>
-            </tr>
+            <thead>
+              <tr>
+                <th>Model</th>
+                <th>Producer</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  {this.state.allCameras[this.state.randomCameraIndex].model}
+                </td>
+                <td>
+                  {this.state.allCameras[this.state.randomCameraIndex].producer}
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
