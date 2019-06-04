836916a1e6b4e4b43e884f6023da6291911e78cc
dwiecko <dwiecko@sigma.ug.edu.pl>
2019-06-04 21:58:38 +0200

    Lab 9 - currency exchange

--- lab9/src/Parent.js
+++ lab9/src/Parent.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Dollar from "./Dollar";
+import PLN from "./PLN";
+
 class Parent extends Component {
   constructor(props) {
     super(props);
@@ -29,3 +31,4 @@ class Parent extends Component {
     );
   }
 }
+export default Parent;
